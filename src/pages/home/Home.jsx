9cfaa163275449bcb483a792cc9536168810dc9b
ziyoviddin <ziyoviddint63@gmail.com>
2025-05-17 15:44:46 +0500

    add

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./Home.css"
+import Card from '../../components/cards/Card'
 function Home() {
   return (
     <>
@@ -10,7 +11,11 @@ function Home() {
         </div>
         <main>
             <section>
-
+           <div className="container">
+           <div className="cards01">
+                <Card/>
+            </div>
+           </div>
             </section>
         </main>
     </>
